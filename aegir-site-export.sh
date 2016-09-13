a2b9245f67bc3b06668c662ac68eaec545d1ae53
James Reidy <jreidy@cornell.edu>
2016-09-13 11:20:19 -0400

    sed - use # as regex delimiter to allow slashes in strings

--- aegir-site-export.sh
+++ aegir-site-export.sh
@@ -55,7 +55,8 @@ sudo -u aegir drush "$TARGET_SITE_ALIAS" sql-dump --ordered-dump --result-file="
 echo 'Converting multi-site to single site...'
 OLDNAME="sites/${TARGET_SITE}"
 NEWNAME="sites/default"
-sed -i.bak -e "s/${OLDNAME}/${NEWNAME}/g" "${DATABASE}"
+echo "sed -i.bak 's#${OLDNAME}#${NEWNAME}#g' ${DUMP_FILE_ABS}"
+sed -i.bak -e "s#${OLDNAME}#${NEWNAME}#g" "${DATABASE}"
 
 # copy the modules, themes, libraries
 echo 'Copying the code...'
